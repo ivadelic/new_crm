34de28fa6f23f0dd2b627f60c316ad1d17c58467
Iva Delic <ms.iva.delic@gmail.com>
2015-04-09 15:21:14 -0400

    Added a new crm and a layout file.

---
+++ contact.rb
@@ -0,0 +1,16 @@
+class Contact
+	attr_accessor :id, :first_name, :last_name, :email, :note 
+	def initialize(first_name, last_name, email, note)
+		@first_name = first_name
+		@last_name = last_name
+		@email = email
+		@note = note 
+	end
+
+	def to_s
+		puts "First Name: #{@first_name} "
+		puts "Last Name: #{@last_name} "
+		puts "Email: #{@email} "
+		puts "Note: #{@note} "
+	end
+end
