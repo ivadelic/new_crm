4ede35e3ba052022245d0a15e46843db9782e4ba
Iva Delic <ms.iva.delic@gmail.com>
2015-04-10 14:38:18 -0400

    Started to initiate my rolodex page

--- crm.rb
+++ crm.rb
@@ -3,6 +3,8 @@ require_relative 'contact'
 require 'sinatra'
 
 $rolodex= Rolodex.new
+@@rolodex= Rolodex.new
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com","Rockstar"))
 
 get '/' do
   erb(:index)
@@ -14,12 +16,17 @@ get "/contacts" do
   erb(:contacts)
 end
 
+get "/contacts/:id" do
+	@contact = @@rolodex.find(1000)
+	erb :show_contact
+end
+
 get '/contacts/new' do
 	erb(:new_contact)
 end
 
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  $rolodex.add_contact(new_contact)
+  @@rolodex.add_contact(new_contact)
   redirect to('/contacts')
 end
